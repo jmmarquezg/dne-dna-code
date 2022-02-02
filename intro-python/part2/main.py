80e6f78e3ed96ea51a4f1dcb475665e1713a1e05
Jose Marquez <josem.mzgz@outlook.com>
2022-02-02 00:51:35 -0600

    Creating main.py to test function deefinition

---
+++ intro-python/part2/main.py
@@ -0,0 +1,8 @@
+def suma(num1, num2):
+    return(num1+num2)
+
+if __name__ == '__main__':
+    print("Este programa calcula la suma de doss numeros enterros\n")
+    a = int(input("Ingrese el primer numero: "))
+    b = int(input("Ingrese el segundo numero: "))
+    print(suma(a,b))
